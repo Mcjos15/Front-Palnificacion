a430aeb7635a0b75b43fbfbddf50db2cafc89df0
Meli <MMCalexandra@gmail.com>
2022-05-28 22:49:33 -0600

    mini update documentos listos para enviar

--- src/components/documents/Documentos.tsx
+++ src/components/documents/Documentos.tsx
@@ -14,62 +14,33 @@ interface Documents {
 
 const Documentos = () => {
 
-    /* 
-    Este metodo no lo uso saco pedazos para convertir a base 64
-    const convertir =(archivos:Blob)=> {
-        console.log(archivos);
-        Array.from(archivos).forEach(archivo=> {
-            var reader = new FileReader();
-            reader.readAsDataURL(archivo);
-            reader.onload = function() {
-               // var arrayAux =[];
-                const base64= reader.result;
-                console.log(base64);
-                //arrayAux = base64.split(',');
-                //console.log(arrayAux[1]);
-            }
-        })
-     } */
-
-
-    const [archivos, setArchivos] = React.useState <Array<File>>([])
+    const [archivos, setArchivos] = React.useState<FileList | null>()
 
     //Obtiene los archivos y se ingresa al useState trato que inserten varios
 
     const subirArchivos = function (e: React.ChangeEvent<HTMLInputElement>) {
 
-        const  fileList  = e.target.files;
+        const fileList = e.target.files;
         console.log(fileList);
         if (!fileList) return;
-        Array.from(fileList).forEach(file => console.log("Do something with " + file.name));
-        
-       // setArchivos(fileList);
+        setArchivos(fileList);
 
     };
     //Se supone que este inserta con el boton 
     const insertArchivos = function (e: React.MouseEvent<HTMLSpanElement, MouseEvent>) {
         console.log(archivos);
-        /* if (archivos) {
-            const formData = new FormData();
-            formData.append("Archivo", archivos, archivos.name);
-            console.log(formData);
+        if (archivos) {
 
-            const reader = new FileReader();
-            reader.readAsDataURL(archivos);
-            reader.onload = function () {
-                var arrayAux = [];
-                const base64 = reader.result;
-                console.log(base64);
+            Array.from(archivos).forEach(archivo => {
+                const reader = new FileReader();
+                reader.readAsDataURL(archivo);
+                reader.onload = function () {
+                    const base64 = reader.result;
+                    //Axios
+                }
 
-
-                //console.log(arrayAux[1]);
-            } */
-
-        //}
-        /*  for(let index = 0; index < archivos.length; index++){
-             datos.append("files",archivos[index]);
-         } */
-
+            })
+        }
     }
 
 
