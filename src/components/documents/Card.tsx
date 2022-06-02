c8ca9e27ef223002932db35fdb217ff09f0e5a05
Mcjos15 <63763240+Mcjos15@users.noreply.github.com>
2022-06-02 17:44:44 -0600

    mini update

--- src/components/documents/Card.tsx
+++ src/components/documents/Card.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import Swal from 'sweetalert2';
 import doc from '../../assets/Image/documento.png'
+import AxiosClient from '../../config/AxiosClient';
+import { render } from '@testing-library/react';
 
 function urltoFile(url: string, filename: string, mimeType: string) {
 
@@ -20,11 +23,36 @@ function urltoFile(url: string, filename: string, mimeType: string) {
 }
 
 
-function Card({ imageSource, title, text, url, base64, type }: any) {
+function Card({ imageSource, id, title, text, url, base64, type, setRender,render }: any) {
     const handleClick = () => {
         if (base64 && type) {
             urltoFile(base64, title, type);
         }
+    }
+    const changeNumber = () => {
+        if (setRender && render) {
+
+            setRender(render +1);
+        }
+    }
+
+    const deleteCard = async () => {
+        console.log(id);
+        await AxiosClient.delete('/api/documents/delete/' + id, { data: { id: id } }).then(res => {
+            console.log(res);
+            if (setRender) {
+
+                changeNumber();
+            }
+        }).catch(error => {
+            Swal.fire({
+                icon: "error",
+                title: error.status,
+                text: error.code,
+            })
+
+        });
+
     }
     return (
         <div className="card text-center bg-dark animate__animated animate__fadeInUp">
@@ -36,10 +64,14 @@ function Card({ imageSource, title, text, url, base64, type }: any) {
                 <p className="card-text text-secondary">
                     {text
                         ? text
-                        : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam deserunt fuga accusantium excepturi quia, voluptates obcaecati nam in voluptas perferendis velit harum dignissimos quasi ex? Tempore repellat quo doloribus magnam."}
+                        : " Sin información."}
                 </p>
                 <button onClick={handleClick}>
                     Descargar documento
+                </button>
+                <br></br>
+                <button onClick={deleteCard}>
+                    Eliminar documento
                 </button>
 
             </div>
--- src/components/documents/Cards.tsx
+++ src/components/documents/Cards.tsx
@@ -6,42 +6,59 @@ import { Documents } from '../../interfaces/Documents';
 import Swal from 'sweetalert2';
 
 interface Card {
-    id?:string,
+    id?: string,
     image: string,
     propietario: string,
     name: string,
     type: string,
     dateCreation: string,
     size: string,
-    base64:string
+    base64: string
 }
 
 
 const cardData: Card[] = [];
 
 function Cards() {
-    const [cards, setCards] = useState<Array<Card>>([])
-    const [documentos, setDocumentos] = React.useState<Documents | null>()
-    useEffect(() => {
+    const [cards, setCards] = useState<Array<Card>>([]);
+    const [render, setRender] = useState(0);
+
+    const changeNumber = () => {
+        if (setRender) {
+
+            setRender(render+1);
+        }
+    }
 
 
-        AxiosClient.get('/api/documents/').then(res => {
+    const getDocuments = async () => {
+
+        await AxiosClient.get('/api/documents/').then(res => {
 
 
             const value = res.data;
-            console.log(value);
+
+            const propietarioLS = (JSON.parse(localStorage.getItem("user")!));
+
+
             for (let i = 0; i < value.length; i++) {
                 console.log(value[i]);
-                cardData.push({
-                    id: value[i].id,
-                    image: doc,
-                    propietario: value[i].propietario,
-                    name: value[i].name,
-                    type: value[i].type,
-                    dateCreation: value[i].dateCreation,
-                    size: value[i].size,
-                    base64:value[i].Base64
-                });
+
+                if (propietarioLS) {
+                    cardData.push({
+                        id: value[i].id,
+                        image: doc,
+                        propietario: propietarioLS!.Name,
+                        name: value[i].name,
+                        type: value[i].type,
+                        dateCreation: value[i].dateCreation,
+                        size: value[i].size,
+                        base64: value[i].Base64
+                    });
+                } else {
+                    console.log("No sirve");
+                }
+
 
             }
             setCards(cardData);
@@ -57,6 +74,12 @@ function Cards() {
             console.log(error);
 
         })
+    }
+    useEffect(() => {
+
+        getDocuments();
+
+
     }, [])
     return (
         <div className="container d-flex justify-content-center h-100">
@@ -66,8 +89,8 @@ function Cards() {
                         return (
                             <div className="col-md-4" key={card.id}>
 
-                                <Card imageSource={card.image} title={card.name} text={card.size+' '+card.propietario}
-                                base64={card.base64} type={card.type}></Card>
+                                <Card imageSource={card.image} title={card.name} text={'peso: ' + card.size + '\n Nombre del propietario ' + card.propietario}
+                                    base64={card.base64} type={card.type} id={card.id} setRender={setRender} render={render}></Card>
                                 <br></br>
                             </div>
                         )
